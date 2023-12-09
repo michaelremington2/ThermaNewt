1d5bfb96dc073a167169ee5f57f944e47c4f2c20
Michael Remington <michaelremington2@gmail.com>
2023-12-09 15:32:45 -0800

    Starte getting the base repository up.

---
+++ setup.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='rattle-newton',
+    version='0.0.1',
+    url='https://github.com/michaelremington2/uumarrty',
+    author='Michael Remington and Jeet Sukumaran',
+    author_email='michaelremington2@gmail.com',
+    license="LICENSE.txt",
+    classifiers=[
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: BSD License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3",
+    ],
+    scripts=[
+        "bin/simulate_uumarrty.py",
+        ],
+    test_suite = "tests",
+    package_dir={"": "src"},
+    description="Ectotherm body temperature simulator using newtons law of cooling.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="src"),
+    python_requires=">=3.8",   
+)
